19d40650c65d5e0ae1f5ea98c0eeb4c1dc3b9535
jerome <hue.jerome@yahoo.fr>
2021-03-06 17:10:23 +0100

    Variable declaration and affectation

--- compiler/visitor.cpp
+++ compiler/visitor.cpp
@@ -1,7 +1,46 @@
 
 // Generated from ifcc.g4 by ANTLR 4.7.2
 
-
 #include "visitor.h"
+#include <string>
+antlrcpp::Any Visitor::visitAxiom(ifccParser::AxiomContext *ctx) 
+ {return visitChildren(ctx);}
 
 
+antlrcpp::Any Visitor::visitProg(ifccParser::ProgContext *ctx) {
+    int retval = stoi(ctx->CONST()->getText()); //gérer erreurs
+    std::cout<<".global main\n"
+           " main: \n"
+           "	#prologue\n"
+           "	pushq %rbp\n"
+           "	movq %rsp, %rbp\n"
+           "\n"
+           "	#body\n";
+    visitChildren(ctx);
+    std::cout <<   
+            "	movl	$"<<retval<<", %eax\n"
+            "\n"
+            "	#epilogue\n"
+            "   popq %rbp\n"
+            " 	ret\n";
+
+     return 0;
+
+ }
+
+
+antlrcpp::Any Visitor::visitAffectation(ifccParser::AffectationContext *ctx){
+    // Checking if variable already exist
+    std::string name = ctx->ID()->getText();
+    if (!symboltable.find(name)) { 
+        std::cout << "La variable a déjà été déclarée" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "        movl    $" 
+            << ctx->CONST()->getText() 
+            << ", " 
+            << symboltable.store(ctx->ID()->getText()) 
+            <<  "(%rbp)\n";
+    return visitChildren(ctx);
+}
--- compiler/visitor.h
+++ compiler/visitor.h
@@ -6,7 +6,7 @@
 
 #include "antlr4-runtime.h"
 #include "antlr4-generated/ifccVisitor.h"
-
+#include "symboltable.h"
 
 /**
  * This class provides an empty implementation of ifccVisitor, which can be
@@ -15,39 +15,13 @@
 class  Visitor : public ifccVisitor {
 public:
 
-  virtual antlrcpp::Any visitAxiom(ifccParser::AxiomContext *ctx) override {
-    return visitChildren(ctx);
-  }
+  virtual antlrcpp::Any visitAxiom(ifccParser::AxiomContext *ctx) override;
 
-  virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override {
-    val_ptr = -4;
-     int retval = stoi(ctx->CONST()->getText()); //gérer erreurs
-     std::cout<<".global main\n"
-           " main: \n"
-           "	#prologue\n"
-           "	pushq %rbp\n"
-           "	movq %rsp, %rbp\n"
-           "\n"
-           "	#body\n";
-    visitChildren(ctx);
-     std::cout <<   
-            "	movl	$"<<retval<<", %eax\n"
-            "\n"
-            "	#epilogue\n"
-            "   popq %rbp\n"
-            " 	ret\n";
+  virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override; 
 
-     return 0;
-  }
-
-  virtual antlrcpp::Any visitAffectation(ifccParser::AffectationContext *ctx) override {
-      std::cout << "        movl    $" << ctx->CONST()->getText() << 
-          ", " << val_ptr <<  "(%rbp)\n";
-      val_ptr = val_ptr -4;
-    return visitChildren(ctx);
-  }
-
+  virtual antlrcpp::Any visitAffectation
+      (ifccParser::AffectationContext *ctx) override;
 protected: 
-  int val_ptr;
+    Symboltable symboltable;
 };
 
