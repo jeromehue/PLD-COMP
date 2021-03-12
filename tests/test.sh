78a97a42197f9c326f76c65566d2f4f4ccbed759
Charly Poirier <pro.poiriercharly@gmail.com>
2021-03-12 17:34:35 +0100

    Comments in test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,10 @@
+# Avoids the "missing folder" error message
 if [ -d "out" ]; then
     rm -Rf "out";
 fi
 
+# Making sure wrapper.sh is executable
 chmod u+x wrapper.sh
+
+# Run tests
 python3 test.py tests/
