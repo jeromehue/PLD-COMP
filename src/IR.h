4a83b0ce1fb4566b554c6a867af6e5611157a027
jerome <hue.jerome@yahoo.fr>
2021-03-27 22:28:42 +0100

    comments

--- src/IR.h
+++ src/IR.h
@@ -176,18 +176,21 @@ class IRInstr {
         returning a boolean value (as an int)
 
 	    Assembly jumps are generated as follows:
-	    BasicBlock::gen_asm() first calls IRInstr::gen_asm() on all 
-        its instructions, and then 
-		    if  exit_true  is a  nullptr, the epilogue is generated
-            else if exit_false is a nullptr, an unconditional jmp 
-                to the exit_true branch is generated
-			else (we have two successors, hence a branch) an instruction 
-            comparing the value of test_var_name to true is generated,
-			followed by a conditional branch to the exit_false branch,
-			followed by an unconditional branch to the exit_true branch
+	    BasicBlock::gen_asm() first calls IRInstr::gen_asm() 
+        on all its instructions, and then 
+		if  exit_true  is a  nullptr, 
+            the epilogue is generated
+        else if exit_false is a nullptr, 
+            an unconditional jmp to the exit_true 
+            branch is generated
+		else (we have two successors, hence a branch) 
+            an instruction comparing the value of test_var_name 
+            to true is generated, followed by a conditional 
+            branch to the exit_false branch, followed 
+            by an unconditional branch to the exit_true branch
 	 
-        The attribute test_var_name itself is defined when converting 
-        the if, while, etc of the AST  to IR.
+        The attribute test_var_name itself is defined 
+        when converting the if, while, etc of the AST  to IR.
 
         Possible optimization:
         a cmp_* comparison instructions, if it is the last 
@@ -222,25 +225,30 @@ class BasicBlock {
 	// No encapsulation whatsoever here. Feel free to do better.
 
 
-	BasicBlock* exit_true;  
     /**< pointer to the next basic block, true branch. 
       If nullptr, return from procedure */ 
+	BasicBlock* exit_true;  
 
 
     /**< pointer to the next basic block, false branch. 
-      If null_ptr, the basic block ends with an unconditional jump */
-	BasicBlock* exit_false; 
+      If null_ptr, the basic block ends with 
+      an unconditional jump */	
+    BasicBlock* exit_false; 
 	
     
-    /**< label of the BB, also will be the label in the generated code */
+    /**< label of the BB, also will be the label 
+      in the generated code */
     std::string label; 
 	
-    CFG* cfg; /** < the CFG where this block belongs */
-	
-    std::vector<IRInstr*> instrs; /** < the instructions themselves. */
-    
-    /**<when generating IR code for an if(expr) or while(expr) etc, 
-        store here the name of the variable that holds the value of expr*/
+    /** < the CFG where this block belongs */
+	CFG* cfg;
+
+    /** < the instructions themselves. */
+    std::vector<IRInstr*> instrs; 
+
+    /**<when generating IR code for an if(expr) or 
+    while(expr) etc, store here the name of the variable 
+    that holds the value of expr*/
     std::string test_var_name;  
  
 
