2eb1d1391d6b06910ddd17c7c4a82f2426f037da
Charly Poirier <pro.poiriercharly@gmail.com>
2021-04-02 15:00:45 +0200

    Added '//' comments in tests

--- tests/tests/comments.c
+++ tests/tests/comments.c
@@ -3,6 +3,6 @@
  * multiple lines.
  */
 int main(/* No parameters */) {
-    /* We are in the main function! */
-    return 0; /* This returns 0 */
+    // We are in the main function!
+    return 0; // This returns 0
 }
