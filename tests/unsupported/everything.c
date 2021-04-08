09362ae94a023d8e49aceba1f4f931c7c28cf136
Charly Poirier <pro.poiriercharly@gmail.com>
2021-04-09 00:27:23 +0200

    Delete tests/unsupported/

--- tests/unsupported/everything.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() { 
-        /** TODO:
-         * Créer un programme avec tout ce qui est
-         * supporté par ifcc...
-         */
-        return 0;        
-}
--- tests/unsupported/while-infinite-loop.c
+++
@@ -1,7 +0,0 @@
-int main() {
-    int a = 10;
-    while (a) {
-        a = a;
-    }
-    return a;
-}
