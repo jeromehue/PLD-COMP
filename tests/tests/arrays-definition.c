56c4078622796bdf5412c879356085b1b886e9ce
Charly Poirier <pro.poiriercharly@gmail.com>
2021-04-09 09:36:56 +0200

    Arrays of type char

--- tests/tests/arrays-definition.c
+++ tests/tests/arrays-definition.c
@@ -2,5 +2,6 @@ int main()
 {
         int a[1] = {0};
         int primes[13] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41};
+        char message[5] = {'H', 'e', 'l', 'l', 'o'};
         return 0;
 }
