c99e3fbfdaeac1d300ba2eed09a3a8bd52dfa791
Charly Poirier <pro.poiriercharly@gmail.com>
2021-04-09 09:56:06 +0200

    int[] to char[] in mainexample.c

--- mainexample.c
+++ mainexample.c
@@ -15,7 +15,7 @@ void display(char c) {
 int main() {
         
         int messageSize = 5;
-        int message[5] = {'H', 'e', 'l', 'l', 'o'};
+        char message[5] = {'H', 'e', 'l', 'l', 'o'};
         
         int i = 0;
         while (i < messageSize) {
